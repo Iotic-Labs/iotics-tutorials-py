50caaff1c7fdf5a84fc3dc7bcd5c5e8c45990945
Lorenzo Paris <lorenzo.paris@iotics.com>
2023-12-12 17:27:49 +0000

    Flight Connector complete

--- examples/iotics-connector-example-flight/src/iotics/connector/example/flight/main.py
+++ examples/iotics-connector-example-flight/src/iotics/connector/example/flight/main.py
@@ -1,24 +1,49 @@
 import logging
 import sys
+from datetime import datetime, timedelta
 
-from airline_connector import AirlineConnector
+import constants as constant
+from flight_connector import FlightConnector
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(message)s",
+    level=logging.getLevelName("INFO"),
+    format="[%(asctime)s] %(levelname)s %(funcName)s %(name)s: %(message)s",
     handlers=[logging.StreamHandler(stream=sys.stdout)],
 )
 
+log = logging.getLogger(__name__)
+
 
 def main():
-    airline_connector_micky_mouse = AirlineConnector(
-        airline_name="micky_mouse",
-        legal_name="Micky Mouse LTD",
-        airline_identifier="AL1",
-        airline_hq_location="UK",
+    flight_connector = FlightConnector()
+    flight_connector.initialise()
+
+    flight_connector.create_new_flight(
+        number="ABC123",
+        departure_airport=constant.AIRPORT_COORDINATES[0],
+        arrival_airport=constant.AIRPORT_COORDINATES[1],
+        departure_time=datetime.now() + timedelta(hours=1),
+        estimated_flight_duration=timedelta(hours=1, minutes=45),
+        airline_identifier=constant.AIRLINE_IDENTIFIERS[0],
     )
-    airline_connector_micky_mouse.initialise()
-    airline_connector_micky_mouse.start()
+    flight_connector.create_new_flight(
+        number="DEF456",
+        departure_airport=constant.AIRPORT_COORDINATES[1],
+        arrival_airport=constant.AIRPORT_COORDINATES[3],
+        departure_time=datetime.now() + timedelta(hours=2),
+        estimated_flight_duration=timedelta(hours=1),
+        airline_identifier=constant.AIRLINE_IDENTIFIERS[1],
+    )
+    flight_connector.create_new_flight(
+        number="GHI789",
+        departure_airport=constant.AIRPORT_COORDINATES[4],
+        arrival_airport=constant.AIRPORT_COORDINATES[0],
+        departure_time=datetime.now() + timedelta(minutes=30),
+        estimated_flight_duration=timedelta(hours=2),
+        airline_identifier=constant.AIRLINE_IDENTIFIERS[2],
+    )
+
+    flight_connector.start()
 
 
 if __name__ == "__main__":
